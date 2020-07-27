4d799b3148ac688bc2f9efd90ad27c74a2aeb6d0
poojasri <poojasrijampani@gmail.com>
2020-07-27 17:13:25 +0530

    update master

--- git.sh
+++ git.sh
@@ -3,8 +3,6 @@ public class gitdemo
 {
   public static void main(String [] args)
   {
-	System.out.println("Hello ignore");
-	System.out.println("Hello develop branch");
-	System.out.println("Hello feature branch");
+	System.out.println("Hello Master");
 }
 }
