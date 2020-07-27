d4dc1b6f23f8e47ef38b548f91abf035f77d76ee
poojasri <poojasrijampani@gmail.com>
2020-07-27 17:00:11 +0530

    .gitignore demo

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ public class gitdemo
 {
   public static void main(String [] args)
   {
-	System.out.println("Hello pooj");
+	System.out.println("Hello ignore");
 	System.out.println("Hello develop branch");
 	System.out.println("Hello feature branch");
 }
