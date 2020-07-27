7d3199a89ccb6ac58ca9c8e8586645e290d255fc
poojasri <poojasrijampani@gmail.com>
2020-07-27 16:18:35 +0530

    update git conflict

--- git.sh
+++ git.sh
@@ -4,10 +4,6 @@ public class gitdemo
   public static void main(String [] args)
   {
 	System.out.println("Hello pooj");
-<<<<<<< HEAD
-	system.out.println("Hello Git bash")
-=======
-	System.out.println("hello conflict")
->>>>>>> 45d293ceb0b46bc8327b3ba8d9c3665207c65600
+	System.out.println("hello git hub conflict")
   }
 }
